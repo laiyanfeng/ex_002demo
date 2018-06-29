0198844150548f9bb3022e6b6a20c21eb5a7bcc7
USER-20170812NL\Administrator <1445652361@qq.com>
2018-06-29 17:24:05 +0800

    first commit

---
+++ ex_002/scripts/test_001.py
@@ -0,0 +1,16 @@
+class TestDemo(object):
+
+    def test_001(self):
+        assert 1
+
+    def test_002(self):
+        assert 0
+
+    def test_003(self):
+        assert 1
+
+    def test_004(self):
+        assert 0
+
+    def test_005(self):
+        assert 1
